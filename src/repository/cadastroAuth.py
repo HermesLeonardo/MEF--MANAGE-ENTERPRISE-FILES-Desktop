8f6c8467b75a26752eaaad548885c2a7ee980fb4
Daniel Guimarães de Faria <danielguimaraesfaria@gmail.com>
2024-10-30 19:45:37 -0300

    Ajustando o caminho do import

--- src/repository/cadastroAuth.py
+++ src/repository/cadastroAuth.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 #from flask import Flask, request, render_template, redirect, url_for
 from firebase_admin import auth
@@ -18,7 +18,7 @@ def cadastroAuth(email, password):
 
 #cadastroAuth('leo@gmail.com', '654321')
 
-#Auth = firebase.auth() 
+Auth = firebase.auth() 
 
 def loginAuth():
     conexaofirebase()
@@ -33,22 +33,3 @@ def loginAuth():
     return
 
 loginAuth()
-
-#API_KEY = credentials.Certificate('C:/UNIFIO-INTEGRADOR/pintregador/pi-6T/testeleo-593ef-firebase-adminsdk-hfnz1-bbcec932a2.json')
-
-    # url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={API_KEY}"
-
-    # payload = {
-    #     "email": email,
-    #     "password": password,
-    #     "returnSecureToken": True
-    # }
-
-    # response = requests.post(url, json=payload)
-
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     print("Sucesso! Usuário autenticado.")
-    #     print("ID Token:", data['idToken'])
-    # else:
-    #     print("Erro na autenticação:", response.json().get("error", {}).get("message", "Erro desconhecido"))
